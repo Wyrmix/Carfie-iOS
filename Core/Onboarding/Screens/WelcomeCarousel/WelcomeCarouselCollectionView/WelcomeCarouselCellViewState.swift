c1a19c685850aaa217c70c1c0ac8d5dfd396a4dc
Christopher Olsen <c.alan.olsen@gmail.com>
2019-11-04 21:51:14 -0600

    Story: Link carousel to webviews (#27)

--- Core/Onboarding/Screens/WelcomeCarousel/WelcomeCarouselCollectionView/WelcomeCarouselCellViewState.swift
+++ Core/Onboarding/Screens/WelcomeCarousel/WelcomeCarouselCollectionView/WelcomeCarouselCellViewState.swift
@@ -10,7 +10,7 @@ import UIKit
 /// Cell type for the welcome carousel. Tells the collection view which class of cell to dequeue.
 enum WelcomeCellType {
     
-    /// A  CollectionViewCell with text, an image, and an action button.
+    /// A  CollectionViewCell with text, an image or animation, and an action button.
     case image
     
     /// A  CollectionViewCell with 2 text labels in large, bold font.
@@ -18,12 +18,32 @@ enum WelcomeCellType {
 }
 
 struct WelcomeCarouselCellViewState: Equatable {
-    let theme: AppTheme
     let cellType: WelcomeCellType
     let topLabelText: String?
     let image: UIImage?
+    let animationName: String?
     let boldText: String
     let bottomLabelText: String?
     let showActionButton: Bool
     let actionButtonLink: String?
+    
+    init(cellType: WelcomeCellType,
+         topLabelText: String? = nil,
+         image: UIImage? = nil,
+         animationName: String? = nil,
+         boldText: String,
+         bottomLabelText: String? = nil,
+         actionButtonLink: String? = nil
+    ) {
+        assert(image == nil  || animationName == nil, "A WelcomeCell cannot display both an animation and an image.")
+        
+        self.cellType = cellType
+        self.topLabelText = topLabelText
+        self.image = image
+        self.animationName = animationName
+        self.boldText = boldText
+        self.bottomLabelText = bottomLabelText
+        self.showActionButton = actionButtonLink == nil ? false : true
+        self.actionButtonLink = actionButtonLink
+    }
 }
