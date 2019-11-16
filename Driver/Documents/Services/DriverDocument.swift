5be8942c41f73bbe3ac8f2b91bd3459e71501144
Christopher Olsen <c.alan.olsen@gmail.com>
2019-11-15 21:29:50 -0600

    Bug: Update driver document ids (#44)

--- Driver/Documents/Services/DriverDocument.swift
+++ Driver/Documents/Services/DriverDocument.swift
@@ -9,20 +9,10 @@
 import Foundation
 
 enum DriverDocumentType: Int, Codable {
-    // These are used by the app currently
-    case driversLicenseFront = 8
-    case driversLicenseBack = 9
-    case vehicleRegistration = 5
-    case insurance = 6
-    case licensePlate = 10
-    
-    // These are not currently used, but are still returned in the response
-    // and thus need to be included to allow decoding.
-    case driversLicense = 1
-    case bankPassbook = 2
-    case joiningForm = 3
-    case workPermit = 4
-    case fitnessCertificate = 7
+    case driversLicenseFront = 1
+    case driversLicenseBack = 2
+    case vehicleRegistration = 3
+    case insurance = 4
 }
 
 struct DriverDocumentList: Codable {
